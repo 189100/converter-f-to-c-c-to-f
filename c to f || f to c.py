30ddae9d179e2c13b81112eaf841e576d744052a
Saksham Maxwell <38845940+189100@users.noreply.github.com>
2018-08-16 07:02:23 +0530

    Add files via upload

---
+++ c to f || f to c.py
@@ -0,0 +1,33 @@
+print("welcome to you in these fucking quiz")
+num1 =int(input("\n what is the number:"))
+one = input("1. in which format do you want to convert\nc to f||f to c:::=>")
+
+
+onea = "c to f"
+print("")
+
+def convert(num1):
+    if one == onea:
+        return ((9*num1)+160)/5
+    else:
+        return ((5*num1)-160)/9
+    
+print(convert(num1))            
+    
+print("")
+print("thanks for using it")
+ 
+    
+    
+
+
+
+
+
+
+    
+
+
+
+
+
